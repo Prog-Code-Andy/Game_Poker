927054b77c3fb632e6029e79dc122f26b4eee1f1
space_irishman <himzpua@gmail.com>
2022-02-05 17:53:47 +0200

    ch

--- poker.js
+++ poker.js
@@ -8,12 +8,14 @@ function Card(suitCard, rangCard){
 
     this.cardPrint = function (desk) {
         if (desk) {
+            console.log(desk);
             let printInf = "";
             for (let i = 0; i < desk.length; i++) {
-                for (let j = 0; j < i.length-1; j++) {
-                    printInf += suit[desk[i][j]] + " " + rang[desk[i][j+1]];
+                for (let j = 0; j < 1; j++) {
+                    printInf += suit[desk[i][j]] + "" + rang[desk[i][j+1]] + " ";
                 }
             }
+            console.log(printInf);
         }
     }
 };
@@ -32,9 +34,31 @@ function Desk(large = 52) {
     this.checkDesk = function () {
         console.log(this.desk);
     }
-}
+
+    this.getCard = function(quantity){
+        if(!quantity || quantity > this.size) return;
+        let numbQua = quantity;
+        let tempQuanArr = new Array();
+        do{
+            let cardRand = Math.floor(Math.random()*this.size);
+            let tepmCardArr = [];
+            if(this.desk[cardRand] === 0){
+                this.desk[cardRand] = 1;
+                tepmCardArr.push(Math.floor(cardRand/13));
+                tepmCardArr.push(cardRand%13);
+                tempQuanArr.push(tepmCardArr);
+                numbQua--;
+            }
+        }while(numbQua >= 1)
+        return tempQuanArr;
+    }
+};
 
 
 let desk = new Desk();
+let card = new Card();
 desk.checkDesk();
+let cardGame = desk.getCard(10);
+cardGame = desk.getCard(10);
+card.cardPrint(cardGame);
 console.log(desk);
