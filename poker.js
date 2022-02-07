8534f16a9112b074e06cc313c32858adfffbc813
space_irishman <himzpua@gmail.com>
2022-02-07 17:53:40 +0200

    07

--- poker.js
+++ poker.js
@@ -2,21 +2,8 @@ function Card(suitCard, rangCard){
     this.suitCard = suitCard;
     this.rangCard = rangCard;
 
-    let suit = ["S","C","D","H"];
-    let rang = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-    
-
-    this.cardPrint = function (desk) {
-        if (desk) {
-            console.log(desk);
-            let printInf = "";
-            for (let i = 0; i < desk.length; i++) {
-                for (let j = 0; j < 1; j++) {
-                    printInf += suit[desk[i][j]] + "" + rang[desk[i][j+1]] + " ";
-                }
-            }
-            console.log(printInf);
-        }
+    this.cardPrint = function () {
+        console.log(this.rangCard + " " + this.suitCard);
     }
 };
 
@@ -26,39 +13,56 @@ function Card(suitCard, rangCard){
 function Desk(large = 52) {
     this.size = large;
     this.desk = new Array(this.size);
+    this.tempQuanArr;
 
     for (let i = 0; i < this.size; i++) {
-        this.desk[i] = 0;
+        this.desk[i] = i;
     }
 
     this.checkDesk = function () {
         console.log(this.desk);
     }
 
+    this
+
     this.getCard = function(quantity){
         if(!quantity || quantity > this.size) return;
         let numbQua = quantity;
-        let tempQuanArr = new Array();
+        this.tempQuanArr = new Array();
         do{
             let cardRand = Math.floor(Math.random()*this.size);
             let tepmCardArr = [];
-            if(this.desk[cardRand] === 0){
-                this.desk[cardRand] = 1;
+            if(this.desk[cardRand] != "*"){
+                this.desk[cardRand] = "*";
                 tepmCardArr.push(Math.floor(cardRand/13));
                 tepmCardArr.push(cardRand%13);
-                tempQuanArr.push(tepmCardArr);
+                this.tempQuanArr.push(tepmCardArr);
                 numbQua--;
             }
         }while(numbQua >= 1)
-        return tempQuanArr;
+        return this.tempQuanArr;
     }
+    
+    this.deskPrint = function () {
+        let suit = ["S","C","D","H"];
+        let rang = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        if (this.tempQuanArr) {
+            let printInf = "";
+            for (let i = 0; i < this.tempQuanArr.length; i++) {
+                for (let j = 0; j < 1; j++) {
+                    printInf += suit[this.tempQuanArr[i][j]] + "" + rang[this.tempQuanArr[i][j+1]] + " ";
+                }
+            }
+            console.log(printInf);
+        }
+    }
+
 };
 
 
 let desk = new Desk();
 let card = new Card();
 desk.checkDesk();
-let cardGame = desk.getCard(10);
-cardGame = desk.getCard(10);
-card.cardPrint(cardGame);
-console.log(desk);
+console.log(desk.getCard(5));
+desk.deskPrint();
+card.cardPrint();
